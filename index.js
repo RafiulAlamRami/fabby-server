62127a9af6dffb59099bfec3832384d0e6a69889
RafiulAlamRami <rafiul7368@gmail.com>
2024-04-29 18:43:55 +0600

    craft details api create

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/craftDetails/:id',async(req,res)=>{
+      const id=req.params.id
+      const query={_id:new ObjectId(id)}
+      const result=await craftCollection.findOne(query)
+      res.send(result)
+    })
+
     app.post('/addCraftItem',async(req,res)=>{
       const newCraftItem=req.body;
       console.log(newCraftItem);
